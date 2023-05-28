53cc8452eeb361b57b2e66a12f9c83db4e4f09da
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-05-29 00:40:25 +0300

    new

--- controllers/superheros/add.js
+++ controllers/superheros/add.js
@@ -1,21 +1,8 @@
-const { db } = require('../../firebase/config');
-const { bucket } = require('../../firebase/config');
-// const { NotFound } = require('http-errors');
-// const { Superhero } = require('../../model');
-// const fs = require('fs/promises');
-// const path = require('path');
-
-// const pathAvatars = path.join(__dirname, '../../public/images');
+const { db } = require("../../firebase/config");
+const { bucket } = require("../../firebase/config");
 
 const updateById = async (req, res) => {
   try {
-    // const docRef = db.collection('superheroes').doc(id);
-    // const doc = await docRef.get();
-
-    // if (!doc.exists) {
-    //   return res.status(404).json({ message: 'Superhero not found' });
-    // }
-
     const imageUrl = await uploadFileToFirebase(req.file);
 
     const superheroData = {
@@ -23,7 +10,7 @@ const updateById = async (req, res) => {
       ...(imageUrl && { heroImage: imageUrl }),
     };
 
-    const docRef = await db.collection('superheroes').add(superheroData);
+    const docRef = await db.collection("superheroes").add(superheroData);
     const docId = docRef.id;
     superheroData.id = docId;
 
@@ -35,20 +22,11 @@ const updateById = async (req, res) => {
       ...doc.data(),
     };
 
-    res.status(201).json({ message: 'Superhero created', data: createdSuperhero });
-    // if (imageUrl) {
-    //   await docRef.update({ ...req.body, heroImage: imageUrl });
-    // }
-    // await docRef.update(req.body);
-    // const updatedDoc = await docRef.get();
-    // const updatedSuperhero = {
-    //   id: updatedDoc.id,
-    //   ...updatedDoc.data(),
-    // };
-
-    // res.json({ message: 'Superhero updated', data: updatedSuperhero });
+    res
+      .status(201)
+      .json({ message: "Superhero created", data: createdSuperhero });
   } catch (error) {
-    res.status(500).json({ message: 'Failed to update superhero' });
+    res.status(500).json({ message: "Failed to update superhero" });
   }
 };
 
@@ -60,19 +38,21 @@ const uploadFileToFirebase = async (file) => {
     await bucket.upload(path, {
       destination,
       metadata: {
-        contentType: 'image/jpeg',
+        contentType: "image/jpeg",
       },
     });
 
     const [uploadedFile] = await bucket.file(destination).get();
 
-    const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(
-      destination
-    )}?alt=media&token=${encodeURIComponent(uploadedFile.metadata.mediaLink.split('&token=')[1])}`;
+    const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${
+      bucket.name
+    }/o/${encodeURIComponent(destination)}?alt=media&token=${encodeURIComponent(
+      uploadedFile.metadata.mediaLink.split("&token=")[1]
+    )}`;
 
     return imageUrl;
   } catch (error) {
-    throw new Error('Erro upload to Firebase: ' + error.message);
+    throw new Error("Erro upload to Firebase: " + error.message);
   }
 };
 
--- controllers/superheros/deleteById.js
+++ controllers/superheros/deleteById.js
@@ -1,24 +1,22 @@
-// const { NotFound } = require('http-errors');
-// const { Superhero } = require('../../model');
-const { db } = require('../../firebase/config');
+const { db } = require("../../firebase/config");
 
 const deleteById = async (req, res) => {
   const { id } = req.query;
-  console.log(id);
+
   try {
-    const docRef = db.collection('superheroes').doc(id);
+    const docRef = db.collection("superheroes").doc(id);
     const doc = await docRef.get();
 
     if (!doc.exists) {
-      return res.status(404).json({ message: 'Superhero not found' });
+      return res.status(404).json({ message: "Superhero not found" });
     }
 
     await docRef.delete();
 
-    res.json({ message: 'Superhero deleted' });
+    res.json({ message: "Superhero deleted" });
   } catch (error) {
-    console.error('Failed to delete superhero:', error);
-    res.status(500).json({ message: 'Failed to delete superhero' });
+    console.error("Failed to delete superhero:", error);
+    res.status(500).json({ message: "Failed to delete superhero" });
   }
 };
 
